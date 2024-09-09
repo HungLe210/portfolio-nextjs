91476f7a17084b9a594367958c74e4e6b00940c0
Le Minh Hung <hungleminh371@gmail.com>
2024-09-09 12:08:40 +0700

    hotfix undefined

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,15 +2,14 @@ import Movies from '@/components/main/Movies';
 
 import ProjectCard from '@/components/sub/ProjectCard';
 import React from 'react'
-import { useGlobalContext } from '@/context/global';
+//import { useGlobalContext } from '@/context/global';
 
 
 
 
 export default function About() {
 
-    const g = useGlobalContext;
-    console.log(g);
+
 
 
     return (
